e51a0b9b4e5bc45d6ba314e2d403424e6ffd7e7b
NitroOxyde <armand@n0s.eu>
2017-03-24 10:59:00 +0100

    Avancement de la gestion de la barre d'url

--- js/dynamicpage.js
+++ js/dynamicpage.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    $(document).ready(function() {
+    if(location.pathname.indexOf(".php") >= 0) // fonction parce que indexOf retourne -1 s'il a rien trouvé
+    {//Si l'url contient .php, on enlève .php
+        history.pushState(null, null, location.pathname.replace(/^.*[\\\/]/, ''));       
+    }
+    else if(location.pathname.length>1)//si l'url contient pas .php on charge la bonne page en rajoutant .php
+    {
+        loadContent(location.pathname+".php");
+    }
+    });
+
     if (window.history && window.history.pushState){
     var newHash      = "",
         $mainContent = $("#main_content"),
