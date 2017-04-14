446409166298219f2bbdc13411e42bd70bacc3c6
NitroOxyde <armand@n0s.eu>
2017-04-14 10:36:15 +0200

    commit pour Hector

---
+++ home.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html> 
+<html>
+	<?php require("require/head.php");?>
+<head>
+		
+</head>
+
+<body id="wrap">
+	<!--NAV BAR-->
+	<?php require("require/nav.php");?>
+
+	<section id="main_content">
+		<div id="guts">
+			<section id="home" class="container-fluid">
+				<div class="row vertical-center">
+					<div class="container text-center">
+						<div class="col">
+							<h1>Cool handmade skateboards.</h1>
+							<hr>
+							<a href="buy.php"><button type="button" class="btn btn-default btn-md">PRE-ORDER</button></a>						
+						</div>
+					</div>	
+				</div>	
+			</section>
+		</div>
+	</section>
+</body>
+
+</html>
