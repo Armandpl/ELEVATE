95418729a1f4c571e27292a577214b24ce966308
Armand <armand@n0s.eu>
2017-05-15 19:33:59 +0000

    découpage du formulaire de commande en 3 parties

--- buy.php
+++ buy.php
@@ -28,46 +28,69 @@
 				                <button type="button" class="btn btn-primary btn-lg" id="order_button">ORDER</button>	
 			                </div>	
 
-		                	<form action="/charge" method="post" class="col-xs-12 col-sm-8 my-auto" id="payment_form" style="display: none;">
-								<div class="form-group">
-								  <label for="name">Name</label>
-								  <input class="form-control form-control-sm" type="text" placeholder="Your name" id="name" required>
+		                	<form action="/charge" method="post" class="col-xs-12 col-sm-8 my-auto" id="payment_form">
+								
+		                		<div id="contact" style="display: none;">
+									<div class="form-group">
+									  <label for="name">Name</label>
+									  <input class="form-control form-control-sm" type="text" placeholder="Your name" id="name" required>
+									</div>
+
+									<div class="form-group">
+									  <label for="surname">Surname</label>
+									  <input class="form-control form-control-sm" type="text" placeholder="Your surname" id="surname" required>
+									</div>
+
+									<div class="form-group">
+									  <label for="email">Email</label>
+									  <input class="form-control form-control-sm" type="email" placeholder="email@example.com" id="email" required>
+									</div>
+
+									<button type="button" class="btn btn-default" id="cancel_button">Cancel</button>
+									<button type="button" class="btn btn-primary" id="contact_next">Next</button>
+								</div>
+								
+								<div id="whole_address" style="display: none;">
+									<div class="form-group">
+									  <label for="country">Country</label>
+									  <input class="form-control form-control-sm" type="text"  id="country" required>
+									</div>
+
+									<div class="form-group">
+									  <label for="address">Address</label>
+									  <input class="form-control form-control-sm" type="text" placeholder="Your adress" id="address" required>
+									</div>
+
+									<div class="form-group">
+									  <label for="city">City</label>
+									  <input class="form-control form-control-sm" type="text" placeholder="Your city" id="city" required>
+									</div>
+
+									<div class="form-group">
+									  <label for="zipcode">Postal/zip code</label>
+									  <input class="form-control form-control-sm" type="text" placeholder="33667" id="zipcode" required>
+									</div>
+
+									<button type="button" class="btn btn-default" id="address_back">Back</button>
+									<button type="button" class="btn btn-primary" id="address_next">Next</button>
 								</div>
 
-								<div class="form-group">
-								  <label for="surname">Surname</label>
-								  <input class="form-control form-control-sm" type="text" placeholder="Your surname" id="surname" required>
+								<div id="payment" style="display: none;">
+									<div class="form-row form-group">
+									  	<label for="card-element">
+									      Credit or debit card
+									    </label>
+									    <div id="card-element" class="form-control form-control-sm">
+									      <!-- a Stripe Element will be inserted here. -->
+									    </div>
+
+									    <!-- Used to display Element errors -->
+									    <div id="card-errors" role="alert"></div>
+									</div>
+
+									<button type="button" class="btn btn-default" id="payment_back">Back</button>
+									<button type="submit" class="btn btn-primary">Submit</button>
 								</div>
-
-								<div class="form-group">
-								  <label for="email">Email</label>
-								  <input class="form-control form-control-sm" type="email" placeholder="email@example.com" id="email" required>
-								</div>
-
-								<div class="form-group">
-								  <label for="surname">Country</label>
-								  <input class="form-control form-control-sm" type="text"  id="surname" required>
-								</div>
-
-								<div class="form-group">
-								  <label for="surname">Address</label>
-								  <input class="form-control form-control-sm" type="text" placeholder="Your adress" id="surname" required>
-								</div>
-
-								<div class="form-row form-group">
-								  	<label for="card-element">
-								      Credit or debit card
-								    </label>
-								    <div id="card-element" class="form-control form-control-sm">
-								      <!-- a Stripe Element will be inserted here. -->
-								    </div>
-
-								    <!-- Used to display Element errors -->
-								    <div id="card-errors" role="alert"></div>
-								</div>
-
-								<button type="button" class="btn btn-default" id="cancel_button">Cancel</button>
-								<button type="submit" class="btn btn-primary">Submit</button>
 							</form>	
 
 		                </div>	
@@ -81,16 +104,39 @@
 	<script type="text/javascript" defer>
 
  	$(document).ready(function() {
+
+ 		$("#country").countrySelect();
  		
  		$("#order_button").click(function() { //Pour afficher le formulaire de paiement lors du clic du bouton order
     		$("#description").hide();
-    		if($(window).width()<576){$("#board_col").hide();}
-    		$("#payment_form").show("slide");
+    		if($(window).width()<576){$("#board_col").hide();}//Si on est sur tel, on cache la board
+    		$("#contact").slideDown();
 		});
+
 		$("#cancel_button").click(function() {
-    		$("#payment_form").hide();
-    		$("#description").show("slide");
-    		if($(window).width()<576){$("#board_col").show("slide");}
+    		$("#contact").hide();
+    		$("#description").slideDown();
+    		if($(window).width()<576){$("#board_col").slideDown();}
+		});
+
+		$("#contact_next").click(function() {
+    		$("#contact").hide();
+    		$("#whole_address").slideDown();
+		});
+
+		$("#address_back").click(function() {
+    		$("#whole_address").hide();
+    		$("#contact").slideDown();
+		});
+
+		$("#address_next").click(function() {
+    		$("#whole_address").hide();
+    		$("#payment").slideDown();
+		});
+
+		$("#payment_back").click(function() {
+    		$("#payment").hide();
+    		$("#whole_address").slideDown();
 		});
 
 		var stripe = Stripe('pk_test_G3PlnRvo1eeDlPEpe4qYoX1P');
--- require/head.php
+++ require/head.php
@@ -2,15 +2,16 @@
 
 	<!--Loading CSS-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/countrySelect.min.css">
 	<link rel="stylesheet" href="css/main.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'  type='text/css'>
 
 	<!--Loading JS-->
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous" defer></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous" defer></script>
-	<!--<script type="text/javascript" src="js/modernizr.js" defer></script>-->
-	<!--<script type="text/javascript" src="js/dynamicpage.js" defer></script>-->
+	<script src="js/countrySelect.min.js" defer></script>
 	<script src="https://js.stripe.com/v3/" defer></script><!--on load stripe partout, c'est mieux pour la détection de fraude-->
 	<script type="text/javascript" src="js/main.js" defer></script>
 
